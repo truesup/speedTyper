b724ba9f353ea1f4aa1d11df3be4fb2f5f7f9097
Pavel Merekin <merekin33@gmail.com>
2025-02-13 14:51:03 +0700

    typing logic and styles change now working

--- src/components/TypingArea.jsx
+++ src/components/TypingArea.jsx
@@ -1,15 +1,39 @@
+import { useRef } from 'react';
 import styles from './TypingArea.module.css';
 
 function TypingArea({ paragraph, typedText, onInputChange }) {
+  const inputRef = useRef(null);
+
   return (
     <div className={styles.typingWrapper}>
       <input
+        ref={inputRef}
         type="text"
+        placeholder="You will type your text here. Just press on this input field or on the text."
         className={styles.input}
         value={typedText}
         onChange={e => onInputChange(e.target.value)}
       />
-      <p className={styles.paragraph}>{paragraph}</p>
+      <p className={styles.paragraph} onClick={() => inputRef.current.focus()}>
+        {paragraph.split('').map((char, index) => {
+          let className = '';
+
+          if (index < typedText.length) {
+            className =
+              typedText[index] === char ? styles.correct : styles.incorrect;
+          }
+
+          if (index === typedText.length) {
+            className += `${styles.active}`;
+          }
+
+          return (
+            <span key={index} className={className}>
+              {char}
+            </span>
+          );
+        })}
+      </p>
     </div>
   );
 }
